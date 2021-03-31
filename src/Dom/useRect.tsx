807a241d7cf3759ccd0c484969f958f58becaf6d
陈晓刚 <chenxiaogang@hupu.com>
2021-03-31 11:31:57 +0800

    feat: 修改

--- src/Dom/useRect.tsx
+++ src/Dom/useRect.tsx
@@ -2,36 +2,31 @@ function isWindow(val: unknown): val is Window {
   return val === window;
 }
 
-const useRect = (
+function makeDOMRect(width: number, height: number) {
+  return {
+    top: 0,
+    left: 0,
+    right: width,
+    bottom: height,
+    width,
+    height,
+  } as DOMRect;
+}
+
+export const useRect = (
   element: (Element | Window | undefined | null)
 ) => {
-
   if (isWindow(element)) {
     const width = element.innerWidth;
     const height = element.innerHeight;
-
-    return {
-      top: 0,
-      left: 0,
-      right: width,
-      bottom: height,
-      width,
-      height,
-    };
+    return makeDOMRect(width, height);
   }
 
   if (element && element.getBoundingClientRect) {
     return element.getBoundingClientRect();
   }
 
-  return {
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    width: 0,
-    height: 0,
-  };
+  return makeDOMRect(0, 0);
 };
 
 export default useRect;
